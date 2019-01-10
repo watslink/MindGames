5048b7448dcc0c0f033616380cfabcf144cd1cf7
watslink <seb.decroix@gmail.com>
2019-01-10 11:28:09 +0100

    creation projet

---
+++ src/com/sd/MindGames/Main.java
@@ -0,0 +1,13 @@
+package com.sd.MindGames;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Bienvenue à MindGames!" 
+                );
+
+
+    }
+}
