900ca2a5beb0aeeebfda31be4480feb602b5e002
watslink <seb.decroix@gmail.com>
2019-01-10 13:35:06 +0100

    ajout des classes AbstractGame, MastermindGame et RechercheGame

--- src/com/sd/MindGames/Main.java
+++ src/com/sd/MindGames/Main.java
@@ -1,13 +1,34 @@
 package com.sd.MindGames;
 
+import java.util.Scanner;
+
 public class Main {
+
+
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Bienvenue à MindGames!"
-                );
+        System.out.println("**************************");
+        System.out.println("**Bienvenue à MindGames!**");
+        System.out.println("**************************");
+        System.out.println("");
+        System.out.println("Listes des jeux:");
+        System.out.println("Recherche + ou - : 1");
+        System.out.println("MasterMind : 2");
+        System.out.println("Votre choix?");
 
+        int nb = sc.nextInt();
+        if (nb != 1 && nb != 2 && nb != 3)
 
+        switch (nb) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+        }
     }
 }
---
+++ src/com/sd/MindGames/games/AbstractGame.java
@@ -0,0 +1,4 @@
+package com.sd.MindGames.games;
+
+public class AbstractGame {
+}
