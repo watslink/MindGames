c09f49416861ef32e11db737542f024f04a246cc
watslink <seb.decroix@gmail.com>
2019-01-14 14:33:32 +0100

    implementation de compareCombinaisons dans MastermindGame

--- src/com/sd/MindGames/games/MastermindGame.java
+++ src/com/sd/MindGames/games/MastermindGame.java
@@ -5,6 +5,8 @@ import com.sd.MindGames.utils.ReadProperties;
 
 public class MastermindGame extends Game {
 
+    private int valeurMalPlacee=0;
+    private boolean valeurMalPlaceeTrouvee;
 
     public MastermindGame(){
         super();
@@ -13,9 +15,59 @@ public class MastermindGame extends Game {
 
 
     @Override
-    public String compareCombinaisons(int[] combinaisonSecrete, int[] reponse) {
-        return null;
+    public String compareCombinaisons(int[] combinaisonSecrete, int[] response) {
+        responseString="Réponse : ";
+        valeurOk=0;
+        valeurMalPlacee=0;
+
+
+        for(int i=0; i<longueurCombinaison;i++)
+        {
+            valeurMalPlaceeTrouvee=false;
+            if (combinaisonSecrete[i]==response[i]) {
+                valeurOk++;
+            }
+            else{
+                for(int j=0; !valeurMalPlaceeTrouvee && j<longueurCombinaison;j++){
+                    if(combinaisonSecrete[j]==response[i]){
+                        valeurMalPlacee++;
+                        valeurMalPlaceeTrouvee=true;
+                    }
+                }
+
+            }
+
+
+        }
+        if(valeurOk==longueurCombinaison){
+            return getVICTORY();
+        }
+        else{
+            switch(valeurMalPlacee){
+                case 0:
+                    break;
+                case 1:
+                    responseString+=valeurMalPlacee+" présent, ";
+                    break;
+                 default:
+                     responseString+=valeurMalPlacee+" présents, ";
+                     break;
+            }
+
+            switch(valeurOk){
+                case 0:
+                    break;
+                case 1:
+                    responseString+=valeurOk+" bien placé";
+                    break;
+                default:
+                    responseString+=valeurOk+" bien placés";
+                    break;
+            }
+
+            return responseString;
     }
 
+}
 
 }
--- src/com/sd/MindGames/modes/Defenseur.java
+++ src/com/sd/MindGames/modes/Defenseur.java
@@ -1,15 +1,33 @@
 package com.sd.MindGames.modes;
 
 import com.sd.MindGames.games.Game;
+import com.sd.MindGames.player.Player;
 
 public class Defenseur extends Mode {
 
+    Player player;
     public Defenseur(Game game){
+
         super(game);
+        player= new Player();
+        start();
     }
 
     @Override
     public void start() {
 
+        System.out.println("Veuillez entrer une combinaison Mystère: ");
+        int [] response=convertStringToTabOfInt(sc.nextLine());
+        if(response!=null){
+            if(checkFormatOfResponse(response)) {
+                player.setCombinaisonSecrete(response);
+            }
+            else
+                System.out.println("Erreur dans la saisie");
+        }
+        else
+            System.out.println("Erreur dans la saisie");
     }
+
+
 }
